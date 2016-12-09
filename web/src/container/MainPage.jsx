e252c33af4038417835e51bb1b356e2d4b3014e5
Kunal Dixit <kunal.dixit@gmail.com>
2016-12-09 17:49:05 -0500

    Local Test

--- web/src/container/MainPage.jsx
+++ web/src/container/MainPage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton/FlatButton';
 import logo from '../images/sugartrek_logo.png';
 import '../styles/index.css';
-
+import gamme from '..sugartrek-unity/test.html';
 class MainPage extends Component {
 
     constructor(props) {
@@ -30,7 +30,7 @@ class MainPage extends Component {
                     <div className="div-logo"><img alt="sugartrek logo" src={logo} /></div>
                     <h2>SugarTrek Game</h2>
                   <div className="div-Game">
-                  <iframe src="http://www.kunal-dixit.com/sugartrek-unity/index.html" width="1150" height="650" frameborder="0" scrolling="no"></iframe>
+                  <iframe src="{game}" width="1150" height="650" frameborder="0" scrolling="no"></iframe>
                   </div>
                 </div>
             </div>
