8d2c950d4bb99aa7a461cc8f1fc9caa867dac74c
kunal <kunal.dixit@gmail.com>
2016-12-09 19:05:58 -0500

    Updated Local Testing

--- web/src/container/MainPage.jsx
+++ web/src/container/MainPage.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton/FlatButton';
 import logo from '../images/sugartrek_logo.png';
+import game from '../sugartrek-unity/game.html';
 import '../styles/index.css';
-import gamme from '..sugartrek-unity/test.html';
+
 class MainPage extends Component {
 
     constructor(props) {
@@ -30,7 +31,9 @@ class MainPage extends Component {
                     <div className="div-logo"><img alt="sugartrek logo" src={logo} /></div>
                     <h2>SugarTrek Game</h2>
                   <div className="div-Game">
-                  <iframe src="{game}" width="1150" height="650" frameborder="0" scrolling="no"></iframe>
+                  <iframe src={game} width="1150" height="650" frameborder="0" scrolling="no"></iframe>
+
+
                   </div>
                 </div>
             </div>
